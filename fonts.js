7371997431cdfe589d2a53de29eb4ee6e92f67eb
LyraMouse <danielcohen116@gmail.com>
2026-05-11 13:32:42 -0400

    Create fonts.js

---
+++ fonts.js
@@ -0,0 +1,8 @@
+export const fonts = [
+  { name: "Rockwell",      css: "'Rockwell', serif" },
+  { name: "Electrolize",   css: "'Electrolize', sans-serif" },
+  { name: "Jolly Lodger",  css: "'Jolly Lodger', cursive" },
+  { name: "Amatic SC",     css: "'Amatic SC', cursive" },
+  { name: "Courier Prime", css: "'Courier Prime', monospace" },
+  { name: "Press Start 2P",css: "'Press Start 2P', monospace" }
+];
